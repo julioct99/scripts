1273af25eac95ebb61651d578cdc662aee2d7547
julioct99 <jcasallot01@gmail.com>
2020-09-14 23:13:35 +0200

    Change format

--- aws-upload.sh
+++ aws-upload.sh
@@ -8,17 +8,18 @@ HOST="18.224.27.200"
 
 if [ $# -ge 1 -a $# -lt 3 ]
 then
-	if [ $# -eq 2 ]
-	then 
-		DEST=$2
-	fi
-	SRC=$1
+  if [ $# -eq 2 ]
+  then 
+    DEST=$2
+  fi
+  SRC=$1
+      
 
-	rsync -av -progress -e \
-		"ssh -i ~/.ssh/julio_docker_test.pem" \
-		$SRC \
-		$USER@$HOST:$DEST
+  rsync -av -progress -e \
+    "ssh -i ~/.ssh/julio_docker_test.pem" \
+    $SRC \
+    $USER@$HOST:$DEST
 else
-	echo "You must provide 1 or 2 parameters"
-	echo "Use: upload src_path [dest_path]"
+  echo "You must provide 1 or 2 parameters"
+  echo "Use: upload src_path [dest_path]"
 fi
