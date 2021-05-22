c09c14bf5a26d546c7c07e889f59c9de95bec450
julioct99 <jcasallot01@gmail.com>
2021-05-22 12:01:40 +0200

    aws-connect: Add datafile as param

--- aws-connect.sh
+++ aws-connect.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 # Open an SSH session into a remote machine
 
-# <-------------------- VARS ----------------------------------------->
+if [ $# -eq 1 ]
+then
+  # <-------------------- VARS ----------------------------------------->
 
-# File to read key, user and host
-DATAFILE="aws.txt"
+  # File to read key, user and host
+  DATAFILE=$1".txt"
 
-# Path to the ssh key
-KEY=$(sed '1q;d' ~/scripts/data/$DATAFILE)
+  # Path to the ssh key
+  KEY=$(sed '1q;d' ~/scripts/data/$DATAFILE)
 
-# User at the remote machine
-USER=$(sed '2q;d' ~/scripts/data/$DATAFILE)
+  # User at the remote machine
+  USER=$(sed '2q;d' ~/scripts/data/$DATAFILE)
 
-# Host at the remote machine
-HOST=$(sed '3q;d' ~/scripts/data/$DATAFILE)
+  # Host at the remote machine
+  HOST=$(sed '3q;d' ~/scripts/data/$DATAFILE)
 
-# <------------------------------------------------------------------>
+  # <------------------------------------------------------------------>
+  ssh -i $KEY $USER@$HOST
+else
+  echo "You must provide 1 parameter"
+  echo "Use: connect [datafile]"
+fi
 
-ssh -i $KEY $USER@$HOST
